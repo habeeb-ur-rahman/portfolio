196d68255b2a10a4efce412dc11707990d16701a
habeeb-ur-rahman <habeeburrahman061@gmail.com>
2022-07-18 02:59:48 +0530

    Updates

---
+++ precache-manifest.1a149164e03b79429b6f18dc0f6ab1ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15245ebf209244abf48117778b5fdabf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3ee49aae6762a04d9fa0",
+    "url": "/portfolio/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "2000e2d04c1099e84aa0",
+    "url": "/portfolio/static/js/2.05d6fa45.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio/static/js/2.05d6fa45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee49aae6762a04d9fa0",
+    "url": "/portfolio/static/js/main.cc4acc04.chunk.js"
+  },
+  {
+    "revision": "c3fe483896071cc46d75",
+    "url": "/portfolio/static/js/runtime-main.75c69042.js"
+  },
+  {
+    "revision": "c4a7a9cc312a76bdade0eca58661e8c2",
+    "url": "/portfolio/static/media/habeeburrahman.c4a7a9cc.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/portfolio/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/portfolio/static/media/resume.9d3da0e2.pdf"
+  }
+]);
